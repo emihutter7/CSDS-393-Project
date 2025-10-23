d542b751a4667e829289f6fc7fe27c5692535aef
raaghuv <raaghuv.vaz@gmail.com>
2025-10-22 20:56:38 -0400

    Typo in Main File fixed

--- scripts/main.py
+++ scripts/main.py
@@ -169,7 +169,7 @@ def show_srv_menu():
         title="Building Menu",
         text="Adjust visual and simulation options",
         user_options=[
-            ("APPLY", ksl_actions),
+            ("APPLY", srv_actions),
             ("CLOSE", lambda: print("Settings closed.")) 
         ],
         user_closable=True
@@ -215,7 +215,7 @@ def show_frib_menu():
 def show_veale_menu():
     global menu_manager
     
-    def ksl_actions():
+    def veale_actions():
         print("placeholder_callback")
         
     veale_menu = Menu(
