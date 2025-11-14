273516e11b74f8c0e57715a3911e27a57bb51c92
ShaneESchwartz <shane.e.schwartz32@gmail.com>
2025-11-14 01:23:13 -0500

    Fixed Agents movement
    Fixed move_along_path() function from Agents class. Also put agents on an actual path.

--- scripts/GameLoop.py
+++ scripts/GameLoop.py
@@ -169,8 +169,23 @@ class StartGame():
         self.menu_manager.main_setup(self.building_data)
 
         # player and agents data
-        self.admins = [Admin(600, random.randint(10, 800), (0, 0, 0), speed=0.05, path_end=(600, 800)) for _ in range(2)]
-        self.students = [Student(600, random.randint(10, 800), (0, 255, 0), speed=0.05, path_end=(600, 830)) for _ in range(4)]
+        # Set up 1 student and 1 admin walking the Glennan <-> Tomlinson road
+
+        self.student = Student(
+            x=336,               # left end of the road
+            y=735,
+            color=(0, 255, 0),
+            speed=0.8,
+            path_end=(756, 735)  # right end of the road
+        )
+
+        self.admin = Admin(
+            x=336,               # right end of the road
+            y=735,
+            color=(0, 0, 0),
+            speed=0.6,
+            path_end=(756, 735)  # left end of the road
+        )
         self.player = Player(200, 300)
         self.player_velocity = [0, 0]
         self.player_input = {"left": False, "right": False, "up": False, "down": False, "select": False}
@@ -292,24 +307,7 @@ class StartGame():
                     btn.handle_event(event)
             
 
-        # agents logic
-        self.students = [Student(600, random.randint(10, 800), (0, 255, 0), speed=0.05, path_end=(600, 830)) for _ in range(4)]
-        self.admins = [Admin(600, random.randint(10, 800), (0, 0, 0), speed=0.01, path_end=(600, 800)) for _ in range(2)]
-
-        self.player_velocity[0] = self.player_input['right'] - self.player_input['left'] # velocity in X direction. If right is true then 1 - 0 = 1, if left is tru thenn 0 - 1 = -1
-        self.player_velocity[1] = self.player_input['up'] - self.player_input['down'] # velocity in Y direction. If up is true then 1 - 0 = 1, if downn is tru thenn 0 - 1 = -1
-
-        self.player_x += self.player_velocity[0] * 5
-        self.player_y += self.player_velocity[1] * 5
-
-        # move & draw AI agents
-        for s in self.students:
-            s.move_along_path()
-            s.draw(self.screen)
-
-        for a in self.admins:
-            a.move_along_path()
-            a.draw(self.screen)
+ 
 
         road_vertical_wyant_to_ksl     = ((525, 70),  (525, 525))
         road_horizontal_ksl_to_glennan = ((525, 525), (780, 525))
@@ -420,6 +418,20 @@ class StartGame():
                 2  # line thickness
             )
 
+        # agents logic
+        self.player_velocity[0] = self.player_input['right'] - self.player_input['left'] # velocity in X direction. If right is true then 1 - 0 = 1, if left is tru thenn 0 - 1 = -1
+        self.player_velocity[1] = self.player_input['up'] - self.player_input['down'] # velocity in Y direction. If up is true then 1 - 0 = 1, if downn is tru thenn 0 - 1 = -1
+
+        self.player_x += self.player_velocity[0] * 5
+        self.player_y += self.player_velocity[1] * 5
+
+        # Move & draw the single student
+        self.student.move_along_path()
+        self.student.draw(self.screen)
+
+        # Move & draw the single admin
+        self.admin.move_along_path()
+        self.admin.draw(self.screen)
 
         # draw all other buttons
         self.next_semester_button.draw(self.screen)
