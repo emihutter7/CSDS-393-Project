0105bb0572910e91814c173aa3089039ca28922b
ShaneESchwartz <shane.e.schwartz32@gmail.com>
2025-11-14 11:29:06 -0500

    Temporary DB workaround
    Restoring game to normal function by temporarily disabling the DB implementation

--- scripts/GameLoop.py
+++ scripts/GameLoop.py
@@ -12,7 +12,8 @@ from .MetricsClass import Metrics
 from .Popups import PopupEvent, minor_events, major_events
 from .menu import Menu, MenuManager
 import numpy as np
-from GameState import GameState
+from .GameState import GameState
+from .db import db_init
 
 # main loop set up, with the screen, clock, and state manager created
 class Game:
@@ -21,7 +22,7 @@ class Game:
         self.screen = py.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) #py.RESIZABLE - if we want to make it full screen, we have to resize EVERYTHING
         self.clock = py.time.Clock()
 
-        db_init()
+        # db_init()
 
         self.current_state = "Main Menu"
         self.font = py.font.SysFont(None, 24)
